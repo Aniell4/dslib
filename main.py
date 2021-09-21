73daff1f7ddecac61a68ff8f5c824ede1837217f
Aniell4 <43622438+Aniell4@users.noreply.github.com>
2021-09-21 22:32:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,57 +1,80 @@
+from typing import Tuple, Union
 import httpx, os, random, string, time, base64
+from dataclasses import dataclass
 
-TOKEN = "ODg5NjM3NzY1MDIzNjgyNjIy.YUkJvQ.d5L6ZxW8JlfEAjdJlYOPK4RhSBc"
-# locked token ODg5NjM3NzU3MjcyNjE2OTYw.YUkJuw.hljp4-vZ7_ifhgmYcAllZbs3YoE
-
-def API(URL):
-    return "https://discord.com/api/v9" + URL
+@dataclass
+class Response: # dataclasses are pretty cool imo, alr have __repr__ in them, so it will show Like <Response {code=200, text='blablabla', json={'gay': 'true'}}>
+    code: int; text: str
+    json: dict = None
 
 class Client(httpx.Client):
-    def __init__(s, Token):
+    def __init__(s, Token: str):
+        '''Discord REST API wrapper.'''
         super().__init__()
         s.headers = GetNewHeaders(Token)
     # https://discord.com/developers/docs/resources/user#get-current-user
     def GetCurrentUser(s):
+        '''Argless User Profile Call'''
         return s.GET('/users/@me')
     # https://discord.com/developers/docs/resources/invite#get-invite
-    def InviteInfo(s, Invite):
+    def InviteInfo(s, Invite: str):
+        '''Only accepts a sanitized code currently.'''
         return s.GET(f'/invites/{Invite}?with_counts=true&with_expiration=true')
-    def JoinInvite(s, Invite):
+    def JoinInvite(s, Invite: str):
+        '''Only accepts a sanitized code currently.'''
         Overwrite = {'x-context-properties': 'eyJsb2NhdGlvbiI6Ik1hcmtkb3duIExpbmsifQ=='}
         return s.POST(f'/invites/{Invite}', headers = Overwrite)
-    def SendMessage(s, Channel, **Message):
+    def SendMessage(s, Channel: str, **Message):
+        '''Requires ChannelId, Use keyword args to pass in msg dict data.'''
         Payload = {"nonce": "".join(random.choice(string.digits) for _ in range(18)), "tts": False}
         Payload.update(Message); return s.POST(f'/channels/{Channel}/messages', json = 
             Payload, headers = {'referer': f'https://discord.com/channels/@me/{Channel}'})
-    def CreateDM(s, User):
+    def CreateDM(s, User: str):
+        '''Requires UserId'''
         return s.POST('/users/@me/channels', json = {'recipients': [User]})
-    def AddUserById(s, UserId):
+    def AddUserById(s, UserId: str):
+        '''Accepts UserIds as strings'''
         Overwrite = {'x-context-properties': 'eyJsb2NhdGlvbiI6IlVzZXIgUHJvZmlsZSJ9'}
         return s.PUT(f'/users/@me/relationships/{UserId}', json = {}, headers = Overwrite)
-    def AddUserByTag(s, UserTag):
+    def AddUserByTag(s, UserTag: str):
+        '''Accepts usertags as strings'''
         Username, Tag = UserTag.split('#')
         Overwrite = {'x-context-properties': 'eyJsb2NhdGlvbiI6IkFkZCBGcmllbmQifQ=='}
         return s.POST(f'/users/@me/relationships', json = {"username": Username, 
             "discriminator": int(Tag)}, headers = Overwrite)
-    def SetUsername(s, Username, Password):
+    def SetUsername(s, Username: str, Password: str): 
+        '''Update Username, Requires Account Password'''
         return s.PATCH('/users/@me', json = {'username': Username, 'password': Password})
     def SetAvatar(s, Picture):
+        '''Update User Avatar with either a Img URL or Local File Object'''
         return s.PATCH('/users/@me', json = {'avatar': ResolveImage(Picture)}, timeout = 100)
+    def SetAboutMe(s, BioText):
+        '''Update User About me'''
+        return s.PATCH('/users/@me', json = {'bio': BioText})
+    def SetHypesquad(s, HypesquadNum: int):
+        '''Update Hypesquad, Accepts Values from 1 to 3'''
+        return s.POST('/hypesquad/online', json = {'house_id': HypesquadNum})
     # Common Methods
     def GET(s, Path, *Args, **Kwargs):
+        '''GET Request using GenReq. Returns Code, Data.'''
         return s.GenReq(s.get, Path, *Args, **Kwargs)
     def POST(s, Path, *Args, **Kwargs):
+        '''POST Request using GenReq. Returns Code, Data.'''
         return s.GenReq(s.post, Path, *Args, **Kwargs)
     def PUT(s, Path, *Args, **Kwargs):
+        '''PUT Request using GenReq. Returns Code, Data.'''
         return s.GenReq(s.put, Path, *Args, **Kwargs)
     def DELETE(s, Path, *Args, **Kwargs):
+        '''DELETE Request using GenReq. Returns Code, Data.'''
         return s.GenReq(s.delete, Path, *Args, **Kwargs)
     def PATCH(s, Path, *Args, **Kwargs):
+        '''PATCH Request using GenReq. Returns Code, Data.'''
         return s.GenReq(s.patch, Path, *Args, **Kwargs)
-    def GenReq(s, Method, Path, *Args, **Kwargs):
+    def GenReq(s, Method, Path, *Args, **Kwargs) -> Response:
+        '''General Purpose API Request Function. Requires a Method and Path. Returns the Status Code, Body.'''
         Y = Method(API(Path), *Args, **Kwargs)
         Code, Data = Y.status_code, Y.json() if Y.headers \
-            ['content-type'] == 'application/json' else Y.text
+            ['content-type'] == 'application/json' else None
         if Code == 429:
             time.sleep(1 + Data['retry_after'] // 995)
             return s.GenReq(Method, Path, *Args, **Kwargs)
@@ -59,11 +82,19 @@ class Client(httpx.Client):
             print('Server error!!')
         elif Code >= 400:
             print(f'Code ({Code}) on', Path, 'using Token:', s.headers['authorization'])
-            if 'message' in Data:
+            if Data and 'message' in Data:
                 print('\tInfo:', Data['message'])
-        return Code, Data
+            else:
+                print('\tUnk:', Y.text)
+        return Response(Code, Y.text, Data)
+
+# Helper Functions
+def API(URL) -> str:
+    '''Resource Path -> Full Discord API URL'''
+    return "https://discord.com/api/v9" + URL
 
 def GetNewHeaders(Token, Referer='https://discord.com/channels/@me'):
+    '''Returns Basic Session Headers'''
     Cookies = [
         ('locale', 'en-GB'),
         ('__dcfduid', RandomX(43)), ('__sdcfduid', RandomX(96)),
@@ -86,20 +117,19 @@ def GetNewHeaders(Token, Referer='https://discord.com/channels/@me'):
     }
 
 def ResolveImage(Var):
+    '''Strings are resolved remotely, File objects locally. Base64 is returned.'''
     Body = httpx.get(Var).content if isinstance(Var, str) else Var.read()
     return "data:image/png;base64," + base64.b64encode(Body).decode('utf-8')
 
 def RandomX(Length):
-	return os.urandom(Length).hex()[Length:]
+    '''Legacy Garbage Hex Function'''
+    return os.urandom(Length).hex()[Length:]
 # Run Stuff
 
+TOKEN = "ODg5NjM3NzY1MDIzNjgyNjIy.YUkJvQ.d5L6ZxW8JlfEAjdJlYOPK4RhSBc"
+# locked token ODg5NjM3NzU3MjcyNjE2OTYw.YUkJuw.hljp4-vZ7_ifhgmYcAllZbs3YoE
+
 C = Client(TOKEN)
-#print(C.SetAvatar('https://cdn.discordapp.com/attachments/888212518437273671/889949943345856532/EbB8Y_aU8AMkI8m.png'))
-#print(C.GetCurrentUser())
-#print(C.SetUsername('Village Rapist', 'LmfaoFuckDiscord123!'))
-# print(C.AddUserByTag('Aniell4#0429'))
-# Code, Data = C.AddUserById('884763385483374662')
-# code, data = C.CreateDM('884763385483374662')
-# print(code, data)
-C.SendMessage('889930594086162495', content = "Hi!")
-# print(C.JoinInvite('5EYE7wap'))
+print(C.GetCurrentUser())
+C.SetAboutMe('sussy baka')
+print(C.GetCurrentUser())
